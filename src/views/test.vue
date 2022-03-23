9219bbe16bf39d3a3c0742f821f5b97dcb848c64
‘e <771500781@qq.com>
2022-03-23 17:45:30 +0800

    fix: transition 报错

--- src/views/test.vue
+++ src/views/test.vue
@@ -1,9 +1,10 @@
-// 跳转这个页面在跳转子页面
+ <!-- 跳转这个页面在跳转子页面 -->
 <template>
+<div>
   just to test
-
   <!-- teleport的用法 -->
   <!-- 可以嵌套到任何dom节点上 -->
   <div id="mainTE">父节点</div>
   <teleport to="#mainTE"><div>子节点</div></teleport>
+  </div>
 </template>
